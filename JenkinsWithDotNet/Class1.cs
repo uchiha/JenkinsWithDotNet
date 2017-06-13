fb488fa0ae9ea325f6d8d74a55110ed89b371dac
DK\clct <clct@sitecore.net>
2017-06-13 17:05:25 +0800

    first commit to test out Jenkins with a simple not net project

---
+++ JenkinsWithDotNet/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsWithDotNet
+{
+    public class Program
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static String CreateMessage()
+        {
+            return "Hello JenkinsCI";
+        }
+    }
+}
